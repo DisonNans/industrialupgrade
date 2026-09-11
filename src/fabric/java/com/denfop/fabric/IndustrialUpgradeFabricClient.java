6c0ae6be54dfa2e1e1ac6ab0a783a23e998ce6c2
DisonNans <89829051+DisonNans@users.noreply.github.com>
2026-09-11 22:37:51 +0300

    Add Fabric client entrypoint

---
+++ src/fabric/java/com/denfop/fabric/IndustrialUpgradeFabricClient.java
@@ -0,0 +1,11 @@
+package com.denfop.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class IndustrialUpgradeFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-only screens, renderers, keybinds and model registration will be
+        // migrated here. No client class is referenced from the common entrypoint.
+    }
+}

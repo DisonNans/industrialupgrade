706ec4890171cd067b7a50a4fa414606e5ef3be5
DisonNans <89829051+DisonNans@users.noreply.github.com>
2026-09-11 22:55:51 +0300

    Register second multi-machine production tier set

--- src/fabric/java/com/denfop/fabric/FabricMultiMachineRegistry.java
+++ src/fabric/java/com/denfop/fabric/FabricMultiMachineRegistry.java
@@ -42,15 +42,39 @@ public final class FabricMultiMachineRegistry {
     public static final Block TRIPLE_EXTRACTOR = machine("triple_extractor", 4, 3);
     public static final Block QUAD_EXTRACTOR = machine("quad_extractor", 4, 4);
 
+    public static final Block DOUBLE_ROLLING = machine("double_rolling", 5, 2);
+    public static final Block TRIPLE_ROLLING = machine("triple_rolling", 5, 3);
+    public static final Block QUAD_ROLLING = machine("quad_rolling", 5, 4);
+    public static final Block DOUBLE_EXTRUDING = machine("double_extruding", 6, 2);
+    public static final Block TRIPLE_EXTRUDING = machine("triple_extruding", 6, 3);
+    public static final Block QUAD_EXTRUDING = machine("quad_extruding", 6, 4);
+    public static final Block DOUBLE_CUTTING = machine("double_cutting", 7, 2);
+    public static final Block TRIPLE_CUTTING = machine("triple_cutting", 7, 3);
+    public static final Block QUAD_CUTTING = machine("quad_cutting", 7, 4);
+    public static final Block DOUBLE_FERMER = machine("double_fermer", 8, 2);
+    public static final Block TRIPLE_FERMER = machine("triple_fermer", 8, 3);
+    public static final Block QUAD_FERMER = machine("quad_fermer", 8, 4);
+    public static final Block DOUBLE_RECYCLER = machine("double_recycler", 9, 2);
+    public static final Block TRIPLE_RECYCLER = machine("triple_recycler", 9, 3);
+    public static final Block QUAD_RECYCLER = machine("quad_recycler", 9, 4);
+    public static final Block DOUBLE_ASSAMPLER_SCRAP = machine("double_assampler_scrap", 10, 2);
+    public static final Block TRIPLE_ASSAMPLER_SCRAP = machine("triple_assampler_scrap", 10, 3);
+    public static final Block QUAD_ASSAMPLER_SCRAP = machine("quad_assampler_scrap", 10, 4);
+
     public static final net.minecraft.block.entity.BlockEntityType<IndustrialProcessorBlockEntity> BLOCK_ENTITY_TYPE =
             FabricBlockEntityTypeBuilder.create(IndustrialProcessorBlockEntity::new,
                     DOUBLE_MACERATOR, TRIPLE_MACERATOR, QUAD_MACERATOR,
                     DOUBLE_COMPRESSOR, TRIPLE_COMPRESSOR, QUAD_COMPRESSOR,
                     DOUBLE_ELECTRIC_FURNACE, TRIPLE_ELECTRIC_FURNACE, QUAD_ELECTRIC_FURNACE,
-                    DOUBLE_EXTRACTOR, TRIPLE_EXTRACTOR, QUAD_EXTRACTOR).build();
+                    DOUBLE_EXTRACTOR, TRIPLE_EXTRACTOR, QUAD_EXTRACTOR,
+                    DOUBLE_ROLLING, TRIPLE_ROLLING, QUAD_ROLLING,
+                    DOUBLE_EXTRUDING, TRIPLE_EXTRUDING, QUAD_EXTRUDING,
+                    DOUBLE_CUTTING, TRIPLE_CUTTING, QUAD_CUTTING,
+                    DOUBLE_FERMER, TRIPLE_FERMER, QUAD_FERMER,
+                    DOUBLE_RECYCLER, TRIPLE_RECYCLER, QUAD_RECYCLER,
+                    DOUBLE_ASSAMPLER_SCRAP, TRIPLE_ASSAMPLER_SCRAP, QUAD_ASSAMPLER_SCRAP).build();
 
     private static boolean initialized;
-
     private FabricMultiMachineRegistry() {}
 
     private static Block machine(String id, int type, int slots) {
@@ -89,13 +113,28 @@ public final class FabricMultiMachineRegistry {
         registerBlock("double_extractor", DOUBLE_EXTRACTOR);
         registerBlock("triple_extractor", TRIPLE_EXTRACTOR);
         registerBlock("quad_extractor", QUAD_EXTRACTOR);
+        registerBlock("double_rolling", DOUBLE_ROLLING);
+        registerBlock("triple_rolling", TRIPLE_ROLLING);
+        registerBlock("quad_rolling", QUAD_ROLLING);
+        registerBlock("double_extruding", DOUBLE_EXTRUDING);
+        registerBlock("triple_extruding", TRIPLE_EXTRUDING);
+        registerBlock("quad_extruding", QUAD_EXTRUDING);
+        registerBlock("double_cutting", DOUBLE_CUTTING);
+        registerBlock("triple_cutting", TRIPLE_CUTTING);
+        registerBlock("quad_cutting", QUAD_CUTTING);
+        registerBlock("double_fermer", DOUBLE_FERMER);
+        registerBlock("triple_fermer", TRIPLE_FERMER);
+        registerBlock("quad_fermer", QUAD_FERMER);
+        registerBlock("double_recycler", DOUBLE_RECYCLER);
+        registerBlock("triple_recycler", TRIPLE_RECYCLER);
+        registerBlock("quad_recycler", QUAD_RECYCLER);
+        registerBlock("double_assampler_scrap", DOUBLE_ASSAMPLER_SCRAP);
+        registerBlock("triple_assampler_scrap", TRIPLE_ASSAMPLER_SCRAP);
+        registerBlock("quad_assampler_scrap", QUAD_ASSAMPLER_SCRAP);
         Registry.register(Registries.BLOCK_ENTITY_TYPE, IndustrialUpgradeFabric.id("industrial_processor"), BLOCK_ENTITY_TYPE);
     }
 
-    private static void registerItem(String path, Item item) {
-        Registry.register(Registries.ITEM, IndustrialUpgradeFabric.id(path), item);
-    }
-
+    private static void registerItem(String path, Item item) { Registry.register(Registries.ITEM, IndustrialUpgradeFabric.id(path), item); }
     private static void registerBlock(String path, Block block) {
         var id = IndustrialUpgradeFabric.id(path);
         Registry.register(Registries.BLOCK, id, block);

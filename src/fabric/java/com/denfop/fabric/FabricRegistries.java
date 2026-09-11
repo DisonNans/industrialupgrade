80e1f5574dced5b0cc781161590b84113c727ed5
DisonNans <89829051+DisonNans@users.noreply.github.com>
2026-09-11 22:45:56 +0300

    Expand standalone material registry for Alloy Smelter

--- src/fabric/java/com/denfop/fabric/FabricRegistries.java
+++ src/fabric/java/com/denfop/fabric/FabricRegistries.java
@@ -13,6 +13,24 @@ import net.minecraft.util.Identifier;
 
 public final class FabricRegistries {
     public static final Item ADVANCED_IRON_INGOT = new Item(new Item.Settings());
+    public static final Item SILVER_INGOT = new Item(new Item.Settings());
+    public static final Item NICKEL_INGOT = new Item(new Item.Settings());
+    public static final Item ZINC_INGOT = new Item(new Item.Settings());
+    public static final Item COPPER_INGOT = new Item(new Item.Settings());
+    public static final Item CHROMIUM_INGOT = new Item(new Item.Settings());
+    public static final Item ALUMINUM_INGOT = new Item(new Item.Settings());
+    public static final Item MAGNESIUM_INGOT = new Item(new Item.Settings());
+    public static final Item TITANIUM_INGOT = new Item(new Item.Settings());
+    public static final Item MANGANESE_INGOT = new Item(new Item.Settings());
+
+    public static final Item ELECTRUM_INGOT = new Item(new Item.Settings());
+    public static final Item INVAR_INGOT = new Item(new Item.Settings());
+    public static final Item RED_BRASS_INGOT = new Item(new Item.Settings());
+    public static final Item NICHROME_INGOT = new Item(new Item.Settings());
+    public static final Item DURALUMIN_INGOT = new Item(new Item.Settings());
+    public static final Item ALUMEL_INGOT = new Item(new Item.Settings());
+    public static final Item FERROMANGANESE_INGOT = new Item(new Item.Settings());
+
     public static final Block ALLOY_SMELTER = new AlloySmelterBlock(Block.Settings.copy(Blocks.IRON_BLOCK));
     public static final net.minecraft.block.entity.BlockEntityType<AlloySmelterBlockEntity> ALLOY_SMELTER_BLOCK_ENTITY =
             FabricBlockEntityTypeBuilder.create(AlloySmelterBlockEntity::new, ALLOY_SMELTER).build();
@@ -26,6 +44,24 @@ public final class FabricRegistries {
         initialized = true;
 
         registerItem("advanced_iron_ingot", ADVANCED_IRON_INGOT);
+        registerItem("silver_ingot", SILVER_INGOT);
+        registerItem("nickel_ingot", NICKEL_INGOT);
+        registerItem("zinc_ingot", ZINC_INGOT);
+        registerItem("copper_ingot", COPPER_INGOT);
+        registerItem("chromium_ingot", CHROMIUM_INGOT);
+        registerItem("aluminum_ingot", ALUMINUM_INGOT);
+        registerItem("magnesium_ingot", MAGNESIUM_INGOT);
+        registerItem("titanium_ingot", TITANIUM_INGOT);
+        registerItem("manganese_ingot", MANGANESE_INGOT);
+
+        registerItem("electrum_ingot", ELECTRUM_INGOT);
+        registerItem("invar_ingot", INVAR_INGOT);
+        registerItem("red_brass_ingot", RED_BRASS_INGOT);
+        registerItem("nichrome_ingot", NICHROME_INGOT);
+        registerItem("duralumin_ingot", DURALUMIN_INGOT);
+        registerItem("alumel_ingot", ALUMEL_INGOT);
+        registerItem("ferromanganese_ingot", FERROMANGANESE_INGOT);
+
         registerBlock("alloy_smelter", ALLOY_SMELTER, true);
         Registry.register(Registries.BLOCK_ENTITY_TYPE, IndustrialUpgradeFabric.id("alloy_smelter"), ALLOY_SMELTER_BLOCK_ENTITY);
     }

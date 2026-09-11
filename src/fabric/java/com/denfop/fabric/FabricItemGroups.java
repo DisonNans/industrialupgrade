aed92ae6a7ecec6b5328bae7598e1b4d0db0c7ea
DisonNans <89829051+DisonNans@users.noreply.github.com>
2026-09-11 22:46:29 +0300

    Expose migrated materials in Industrial Upgrade item group

--- src/fabric/java/com/denfop/fabric/FabricItemGroups.java
+++ src/fabric/java/com/denfop/fabric/FabricItemGroups.java
@@ -8,9 +8,28 @@ import net.minecraft.util.Identifier;
 
 public final class FabricItemGroups {
     public static final ItemGroup INDUSTRIAL_UPGRADE = FabricItemGroup.builder()
-            .icon(() -> new ItemStack(net.minecraft.item.Items.IRON_INGOT))
+            .icon(() -> new ItemStack(FabricRegistries.ADVANCED_IRON_INGOT))
             .displayName(Text.translatable("itemGroup.industrialupgrade.main"))
-            .entries((context, entries) -> { })
+            .entries((context, entries) -> {
+                entries.add(new ItemStack(FabricRegistries.ADVANCED_IRON_INGOT));
+                entries.add(new ItemStack(FabricRegistries.SILVER_INGOT));
+                entries.add(new ItemStack(FabricRegistries.NICKEL_INGOT));
+                entries.add(new ItemStack(FabricRegistries.ZINC_INGOT));
+                entries.add(new ItemStack(FabricRegistries.COPPER_INGOT));
+                entries.add(new ItemStack(FabricRegistries.CHROMIUM_INGOT));
+                entries.add(new ItemStack(FabricRegistries.ALUMINUM_INGOT));
+                entries.add(new ItemStack(FabricRegistries.MAGNESIUM_INGOT));
+                entries.add(new ItemStack(FabricRegistries.TITANIUM_INGOT));
+                entries.add(new ItemStack(FabricRegistries.MANGANESE_INGOT));
+                entries.add(new ItemStack(FabricRegistries.ELECTRUM_INGOT));
+                entries.add(new ItemStack(FabricRegistries.INVAR_INGOT));
+                entries.add(new ItemStack(FabricRegistries.RED_BRASS_INGOT));
+                entries.add(new ItemStack(FabricRegistries.NICHROME_INGOT));
+                entries.add(new ItemStack(FabricRegistries.DURALUMIN_INGOT));
+                entries.add(new ItemStack(FabricRegistries.ALUMEL_INGOT));
+                entries.add(new ItemStack(FabricRegistries.FERROMANGANESE_INGOT));
+                entries.add(new ItemStack(FabricRegistries.ALLOY_SMELTER.asItem()));
+            })
             .build();
 
     private FabricItemGroups() {}
